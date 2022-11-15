95c69dccae9b9f1e57285913324d009ae8a18647
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-15 20:34:56 +0100

    adapter and serializer

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,10 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model() {
-    const response = await fetch('/api/rentals.json');
-    const { data } = await response.json();
-    return data.map((model) => {
-      const { id, attributes } = model;
-
-      const type = COMMUNITY_CATEGORIES.includes(attributes.category)
-        ? 'Community'
-        : 'Standalone';
-
-      return { type, ...attributes, id };
-    });
+    return this.store.findAll('rental');
   }
 }
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,18 +1,10 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
 
 export default class RentalRoute extends Route {
+  @service store;
+
   async model(params) {
-    const response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    const { data } = await response.json();
-
-    const { id, attributes } = data;
-
-    const type = COMMUNITY_CATEGORIES.includes(attributes.category)
-      ? 'Community'
-      : 'Standalone';
-
-    return { id, type, ...attributes };
+    return this.store.findRecord('rental', params.rental_id);
   }
 }
